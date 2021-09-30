a840aaf0f1925909a5ee5e92a3fb0e4d222eb4c9
Matthew Rudd <mbrudd@sewanee.edu>
2021-09-30 09:07:30 -0500

    updates

---
+++ _site/stat204/lectures/chap4D.R
@@ -0,0 +1,39 @@
+# Exploring binomial and noral probability distributions
+
+library(tidyverse)
+library(tableone)
+
+# chance of 10 heads in 20 flips of a fair coin:
+dbinom( x=10, size=20, prob=0.5 )
+
+# chance of 7 heads in 20 flips of a fair coin:
+dbinom( x=7, size=20, prob=0.5 )
+
+# chance of 10 heads in 20 flips of an unfair coin:
+dbinom( x=10, size=20, prob=0.1 )
+
+# chance of getting 9 or fewer heads in 20 flips of a fair coin:
+pbinom( q=9, size=20, prob=0.5) 
+
+coin_flips <- data.frame( successes = 0:20, probs = dbinom(0:20, size=20, prob=0.5))
+ggplot( coin_flips, aes(x=successes, y=probs)) + geom_col()
+
+coin_flips2 <- data.frame( successes = rbinom(n=1000, size=20, prob=0.5))
+ggplot(coin_flips2, aes(x=successes)) + geom_bar()
+
+unfair_flips <- data.frame( successes = 0:20, probs = dbinom(0:20, size=20, prob=0.1))
+ggplot( unfair_flips, aes(x=successes, y=probs)) + geom_col()
+
+unfair_flips2 <- data.frame( successes = rbinom(n=1000, size=20, prob=0.1))
+ggplot(unfair_flips2, aes(x=successes)) + geom_bar() + xlim(0,20)
+
+# reproduce Figure 4.6 :
+
+pdmp_sample <- data.frame( n_states = 0:20, probs = dbinom( 0:20, size=20, prob=0.51)) 
+ggplot( pdmp_sample , aes(x=n_states, y=probs)) + 
+  geom_col(fill="purple") + 
+  labs(x="States with monitoring programs")
+
+opioid.policy.kff <- read_csv("pdmp_2017_kff_ch4.csv")
+opioid.policy.kff <- opioid.policy.kff %>% rename( State = "...1" )
+CreateTableOne(data = opioid.policy.kff)
